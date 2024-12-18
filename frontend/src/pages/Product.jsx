91ca4935e7a58d1e3f10e5b6f10271787579e3b7
Nur Ahmed Omar <nuuraxmed.8804@gmail.com>
2024-12-18 07:23:24 +0300

    Add Bakaro homepage

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,19 +2,21 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const {productId} = useParams(); 
-  const {products} = useContext(ShopContext);
+  const {products, currency, addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('')
+  const [size, setSize] = useState('')
 
   const fetchProductData = async () => {
     products.map((item)=>{
       if(item._id === productId){
         setProductData(item)
         setImage(item.image[0])
-        console.log(item);
+
         
         return null;
       }
@@ -55,10 +57,38 @@ const Product = () => {
             <img src={assets.star_dull_icon} alt="" className="w-3 5" />
             <p className='pl-2'>(122)</p>
           </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5 '>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8 '>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+              {productData.sizes.map((item, index)=>(
+                <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
+          <button onClick={()=>addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5'/>
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
-
-
       </div>
+      {/* Description & Review Section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eveniet nisi ratione vero iste, autem error at temporibus doloremque exercitationem quis sequi blanditiis iusto nobis, necessitatibus laborum enim corrupti alias?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed tenetur recusandae cum cupiditate nemo unde, magnam fugit natus praesentium quod sunt quibusdam possimus rem hic sit accusantium, perferendis accusamus commodi!</p>
+        </div>
+      </div>
+      {/* Display related products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div className='opacity-0'></div>
 }
